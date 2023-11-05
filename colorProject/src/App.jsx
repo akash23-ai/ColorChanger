abdc31e9b954bc450c93122c5aee04a310135b3b
akash23-ai <akashbhardwaj2024@gmail.com>
2023-11-05 22:49:36 +0530

    first commit

---
+++ colorProject/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="flexBox">
+
+      <Card prop={'Lamborgini'}/>
+      <Card prop={'Porsche'}/>
+      <Card prop={'BMW'}/>
+    </div>
+  );
+}
+
+export default App;
